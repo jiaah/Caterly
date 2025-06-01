0902ee92fff801072c83aa429efbc61b5a026ba6
jiaah <25231717+jiaah@users.noreply.github.com>
2025-06-01 23:37:57 +0900

    feat(design): create Button component with variant

--- packages/design/src/components/ui/button.tsx
+++ packages/design/src/components/ui/button.tsx
@@ -4,19 +4,18 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 
 const buttonVariants = cva(
-	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all border border-transparent disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:inset-ring-2 aria-invalid:inset-ring-2",
 	{
 		variants: {
 			variant: {
-				default: 'bg-primary text-primary shadow-xs hover:bg-primary/90',
-				destructive:
-					'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
+				default: 'bg-primary text-on-bg shadow-xs hover:bg-primary-hover',
+				secondary: 'bg-secondary text-on-bg shadow-xs hover:bg-secondary-hover',
+				destructive: 'bg-error text-on-bg shadow-xs hover:bg-error-hover',
 				outline:
-					'border bg-primay shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
-				secondary: 'bg-secondary text-base shadow-xs hover:bg-secondary/80',
+					'border-base bg-surface shadow-xs hover:bg-surface-muted hover:text-primary hover:border-primary dark:bg-surface/20 dark:border-accent dark:text-soft-white dark:hover:bg-surface-muted/80',
 				ghost:
-					'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
-				link: 'text-primary underline-offset-4 hover:underline',
+					'text-base hover:bg-surface-muted hover:text-primary dark:hover:bg-surface-muted/80',
+				link: 'text-base underline-offset-4 hover:underline hover:text-primary',
 			},
 			size: {
 				default: 'h-9 px-4 py-2 has-[>svg]:px-3',
